9e2cd4891f0e15dd77b8767dfbd87ba380be3dda
dengzemiao <dengzemiaovip@163.com>
2023-08-03 11:37:09 +0800

    2023-08-03 11:37:09 提交优化

---
+++ njoyd/ggit/src/README.md
@@ -0,0 +1,279 @@
+## 一、简介
+
+- 在开发过程中，习惯用 `Git 命令` 提交、合并代码的小伙伴会发现，经常在重复的敲提交代码就算了，尤其是测试环节，修好之后，来回切换提交、合并操作，所以这也是很多小伙伴选择用可视化操作的原因。
+
+- 所以使用 `Node` 对 `Git` 相关命令进行包装，做了一个自动化命令工具，在自动化命令的基础上，还额外支持所有不带参的 `git` 原生命令，下面使用中有说明什么叫不带参命令。
+
+- 在执行 `@nj/ggit` 自动化命令过程中遇到冲突，解决后，可继续执行一遍相关命令 `push， merge` 完成提交。
+
+## 二、安装
+
+- 请务必提前安装好 `Git` 、 `Node`。
+
+  `Windows` 包管理工具：[winget](https://blog.csdn.net/zz00008888/article/details/131091717)
+
+  `Mac` 包管理工具：[homebrew](https://blog.csdn.net/zz00008888/article/details/113867051)
+
+  `Linux` 包管理工具：`Yum`
+
+  各平台都可以通过包管理工具快速安装 `Git` 、`Node`。
+
+- 将 `@nj/ggit` 安装到全局 (`小提示：记得切换为私有源再进行安装`)
+
+  ```sh
+  # 安装并切换好了可忽略这一步
+  # 安装 nrm 管理镜像源
+  $ npm install -g nrm
+  # 添加私有镜像源
+  $ nrm add nnpm http://10.0.10.23:7001
+  # 使用私有镜像源
+  $ nrm use nnpm
+  ```
+
+  ```sh
+  $ npm i -g @nj/ggit
+  ```
+
+  ```sh
+  $ ggit -v
+  ```
+
+  如果安装失败，可将镜像切换到官方镜像尝试
+
+  ```sh
+  # 查看镜像
+  $ npm get registry
+
+  # 切换为官方镜像
+  $ npm config set registry https://registry.npmjs.org
+
+  # 切换为淘宝镜像
+  $ npm config set registry https://registry.npm.taobao.org
+  ```
+
+- 可通过 `ggit -h` 查看帮助文档，查看支持子命令与参数
+
+  ```sh
+  $ ggit -h
+  ```
+
+  ```sh
+  # 输出
+  Usage: ggit [options] [command]
+
+  # ggit 支持的参数
+  Options:
+    -V, --version     output the version number
+    -v                output the version number
+    -d [branch]       移除指定本地分支
+    -dr [branch]      移除指定远程分支
+    -b [branch]       以当前分支为基础，新建分支
+    -g, --go [branch] 切换到指定分支，如本地没有会拉取远程分支
+    -h, --help        display help for command
+
+  # ggit 支持的子命令
+  Commands:
+    pull             同步当前分支的远程数据，如果未建立远程分支跟踪，会自动跟踪远程分支，并拉取最新数据
+    push [options]   提交当前分支到远程仓库，并可在提交完成后，自动切换到指定分支
+    merge [options]  提交当前分支到远程仓库，并合并到指定分支，再返回当前分支/指定分支
+    fix [options]    目前主要作用于修复分支偏移问题
+    help [command]   display help for command
+  ```
+
+- 可通过 `ggit push|merge|fix -h` 查看帮助文档，查看子命令支持参数
+
+  ```sh
+  $ ggit push -h
+  ```
+
+  ```sh
+  Usage: ggit push [options]
+
+  提交当前分支到远程仓库
+
+  # push 支持的参数
+  Options:
+    -g, --go [branch]    合并提交结束后，切换到指定分支
+    -s, --stash [type]   使用 stash 暂存区方式合并代码，如手动终止脚本、执行失败停止脚本，需检查是否执行了 $ git stash pop 命令，没有执行需要手动执行放出暂存区的代码，以免丢失 (default: true)
+    -m, --message [msg]  提交日志信息 (default: "2023-06-09 11:32:11 提交优化")
+    -h, --help           display help for command
+  ```
+
+## 三、使用
+
+- `ggit` 支持的属性
+
+  ```sh
+  # 切换到指定分支
+  $ ggit -g 2.3.4
+
+  # 移除远程分支
+  $ ggit -dr 2.3.4
+
+  ....
+  ```
+
+  | `ggit`            | 含义                                     | 支持传参 | 默认值 |
+  | ----------------- | ---------------------------------------- | -------- | ------ |
+  | -v \| -V          | 查看版本                                 | 无       | 无     |
+  | -g, --go [branch] | 切换到指定分支，如本地没有会拉取远程分支 | 无       | 无     |
+  | -b [branch]       | 以当前分支为基础，新建分支               | 无       | 无     |
+  | -d [branch]       | 移除指定本地分支                         | 无       | 无     |
+  | -dr [branch]      | 移除指定远程分支                         | 无       | 无     |
+
+- `ggit` 目前支持的子命令
+
+  ```sh
+  # 1、拉取当前分支远程代码，会自动建立远程分支跟踪
+  $ ggit pull
+
+  # 2、提交当前分支代码
+  $ ggit push
+
+  # 3、合并代码，提交当前分支代码，并合并到默认分支 dev，在切回当前分支继续开发
+  $ ggit merge === $ ggit merge -t dev
+
+  # 4、修复分支偏移，一般情况不需要，看到报错分支偏移时可以使用
+  $ ggit fix
+
+  # 5、修改默认配置
+  # 比如 merge 默认合并的是 dev 分支，如果想默认是的 master，则可以通过 cset 修改，这样就不需要在 $ ggit merge 时不需要指定 -t master，其他配置同理。
+  $ ggit config set -t master
+
+  # 6、获取所有默认配置
+  $ ggit config === $ ggit config get
+  # 获取指定配置
+  $ ggit config -t === $ ggit config get -t
+  ```
+
+- 子命令参数介绍
+
+  | `pull` | 含义 | 支持传参 | 默认值 |
+  | ------ | ---- | -------- | ------ |
+  | 无     | 无   | 无       | 无     |
+
+  | `push`               | 含义                                                                                                                                                    | 支持传参                  | 默认值              |
+  | -------------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------- | ------------------------- | ------------------- |
+  | -g, --go \[branch]   | 合并提交结束后，切换到指定分支                                                                                                                          | 分支名称                  | -                   |
+  | -s, --stash \[type]  | 使用 stash 暂存区方式合并代码，如手动终止脚本、执行失败停止脚本，需检查是否执行了 \$ git stash pop 命令，没有执行需要手动执行放出暂存区的代码，以免丢失 | 0/1/true/false/Ture/False | true                |
+  | -m, --message \[msg] | 提交日志信息                                                                                                                                            | -                         | 当前时间 + 提交优化 |
+
+  | `merge`              | 含义                                                                                                                                                    | 支持传参                  | 默认值              |
+  | -------------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------- | ------------------------- | ------------------- |
+  | -t, --to \[branch]   | 合并到指定的分支                                                                                                                                        | 分支名称                  | dev                 |
+  | -g, --go \[branch]   | 合并提交结束后，切换到指定分支                                                                                                                          | 分支名称                  | 当前分支            |
+  | -s, --stash \[type]  | 使用 stash 暂存区方式合并代码，如手动终止脚本、执行失败停止脚本，需检查是否执行了 \$ git stash pop 命令，没有执行需要手动执行放出暂存区的代码，以免丢失 | 0/1/true/false/Ture/False | true                |
+  | -m, --message \[msg] | 提交日志信息                                                                                                                                            | -                         | 当前时间 + 提交优化 |
+
+  | `fix`                | 含义                             | 支持传参                  | 默认值                  |
+  | -------------------- | -------------------------------- | ------------------------- | ----------------------- |
+  | -p, --push \[type]   | 修复完成后，将代码提交到远程分支 | 0/1/true/false/Ture/False | true                    |
+  | -g, --go \[branch]   | 合并提交结束后，切换到指定分支   | 分支名称                  | -                       |
+  | -m, --message \[msg] | 提交日志信息                     | -                         | 当前时间 + 修复分支偏移 |
+
+  | `config`            | 含义                          | 支持传参                               | 默认值 |
+  | ------------------- | ----------------------------- | -------------------------------------- | ------ |
+  | set                 | 修改默认配置                  | 必传（例：`$ ggit config set -t dev`） | -      |
+  | get                 | 查看默认配置                  | 必传（例：`$ ggit config get -t`）     | dev    |
+  | -t, --to \[branch]  | 合并到指定的分支              | 分支名称                               | dev    |
+  | -s, --stash \[type] | 是否使用 stash 暂存区合并状态 | 0/1/true/false/Ture/False              | true   |
+
+- 子命令参数使用
+
+  例如：当前在 2.0 分支，需要直接提交当前分支，使用默认备注即可。
+
+  ```sh
+  $ ggit push
+  ```
+
+  例如：当前在 2.0 分支，需要带上提交备注，提交当前分支到远程。
+
+  ```sh
+  $ ggit push -m "优化代码"
+
+  或
+
+  $ ggit push --message '优化代码'
+  ```
+
+  例如：当前在 2.0 分支，需要带上提交备注，提交当前分支到远程，提交完成后切换到 1.0 分支。
+
+  ```sh
+  $ ggit push -m '优化代码' -g 1.0
+  ```
+
+- 扩展性，除了支持上面的子命令外，额外支持全部 `git` 命令，但是不支持带参数的命令：
+
+  ```sh
+  $ ggit add .               =       $ git add .
+  $ ggit commit -m '备注'     =       $ git commit -m '备注'
+  $ ggit pull                =       $ git pull
+  ....
+  # 基本所有不带参的 git 命令都支持
+
+  # 只有执行 push、merge 命令时会被 ggit 拦截，并使用 ggit 的 push、merge
+  $ ggit push origin master  =       执行的是 $ ggit push，而不是 $ git push，多余的参数会不生效，merge 同理，其他命令都不会被拦截，因为不同名。
+  ```
+
+  ```sh
+  # 什么叫带参命令，例如：
+  $ git branch --set-upstream-to=origin/<分支> 5.0
+
+  # 如果确实要用，肯定不如直接使用 git，但是也能用，使用 '' 或 "" 包裹一下即可
+  # $ ggit '包裹的命令'
+  $ ggit 'branch --set-upstream-to=origin/<分支> 5.0'
+
+  # -- 或 - 开头的就是参数，不支持 git 的这种带参命令，不带参数的都支持
+  ```
+
+## 四、与 `Git` 原生命令对比
+
+- 提交代码
+
+  ```sh
+  # 原生
+  $ git add .
+  $ git commit -m '优化代码'
+  $ git pull origin 当前分支
+  $ git push origin 当前分支
+  ```
+
+  ```sh
+  # ggit
+  $ ggit push -m "优化代码"
+
+  # 如果不需要备注，使用默认备注
+  $ ggit push
+  ```
+
+- 修复 `BUG`，提交当前分支，合并到 `dev` 分支，并切回当前开发分支或其他分支
+
+  ```sh
+  # 原生
+  $ git add .
+  $ git commit -m '优化代码'
+  $ git pull origin 开发分支
+  $ git push origin 开发分支
+  $ git checkout dev
+  $ git pull origin dev
+  $ git merge 开发分支
+
+  # 在这环节，ggit 如果 merge 遇到冲突，解决后，继续执行一遍 merge 命令，也能完成提交代码，加上 -g 就能再次回到指定分支
+  # 也可以使用 push 命令上传，原生命令就还的 add 敲一遍。
+
+  $ git push origin dev
+  $ git checkout 开发分支/指定分支
+  ```
+
+  ```sh
+  # ggit
+
+  # 提交合并完成回到当前分支（-g 默认当前分支）
+  $ ggit merge -m "优化代码"
+
+  # 提交合并完成回到指定分支
+  $ ggit merge -m "优化代码" -g 1.0
+
+  # 如果不需要备注，使用默认备注
+  $ ggit merge
+  ```
---
+++ njoyd/ggit/src/index.js
@@ -0,0 +1,581 @@
+#!/usr/bin/env node
+
+// 处理命令
+const shell = require('shelljs')
+// 处理输出
+const chalk = require('chalk')
+// 处理文件
+const fs = require('fs')
+// 路径
+const path = require('path')
+// 配置
+const config = require('./config.json')
+// 命令行辅助
+const { program } = require('commander')
+// 当前版本
+const { version } = require('./package.json')
+
+// ================================================== 公共处理
+
+// 执行命令，控制台【不输出结果只返回，检查命令是否执行结束】
+function execSilentCheck(cmd, config) {
+  // 返回结果
+  return exec(cmd, { silent: true, check: true, ...config })
+}
+// 执行命令，控制台【不输出结果只返回，不检查命令是否执行结束】
+function execSilent(cmd, config) {
+  // 返回结果
+  return exec(cmd, { silent: true, ...config })
+}
+// 执行命令，控制台【输出结果并返回，检查命令是否执行结束】
+function execCheck(cmd, config) {
+  // 返回结果
+  return exec(cmd, { check: true, ...config })
+}
+// 执行命令，控制台【输出结果并返回，不检查命令是否执行结束】
+function exec(cmd, config) {
+  // 配置
+  const conf = config || {}
+  // 执行命令
+  const res = shell.exec(cmd, {
+    // 不输出结果
+    silent: conf.silent
+  })
+  // 执行失败
+  if (res.code != 0 && conf.check) {
+    // 如果处于禁止输出状态，强行输出错误结果
+    if (conf.silent) {
+      // 输出错误结果
+      console.log(rmEnter(res.stdout))
+    }
+    // 输出错误提示
+    showError(cmd, conf.errMsg)
+    // 结束脚本提示
+    if (conf.exitMsg) {
+      // 以错误结果展示
+      BgError(conf.exitMsg)
+    }
+    // 停止脚本
+    if (conf.exit === undefined || conf.exit === true) {
+      exit(res.code)
+    }
+  }
+  // 返回结果
+  return res
+}
+// 结束脚本 code: 0 结束 1 失败
+function exit(code) {
+  process.exit(code)
+}
+// 显示错误
+function showError(cmd, msg) {
+  // 错误消息
+  const errMsg = msg || `${cmd} 执行失败`
+  // 输出错误提示
+  BgError(`========================================== Error：${errMsg}`)
+}
+// 移除头尾空格与换行符
+function rmEnter(text) {
+  return (text || '').trim()
+}
+// 判断值是否为true
+function isTure(value) {
+  // 支持 1、'1'、'true'、'Ture'
+  return value == 1 || value === true || value == 'true' || value == 'Ture'
+}
+
+// 输出
+// const Info = (msg) => { console.log(chalk.blueBright.bold(msg)) }
+// const Error = (msg) => { console.log(chalk.redBright.bold(msg)) }
+// const Success = (msg) => { console.log(chalk.greenBright.bold(msg)) }
+// const Warning = (msg) => { console.log(chalk.hex('#FFA500').bold(msg)) }
+
+// 输出(带背景)
+const BgInfo = (msg) => { console.log(chalk.bgBlue.bold(msg)) }
+const BgError = (msg) => { console.log(chalk.bgRed.bold(msg)) }
+const BgSuccess = (msg) => { console.log(chalk.bgGreenBright.bold(msg)) }
+const BgWarning = (msg) => { console.log(chalk.bgHex('#FFA500').bold(msg)) }
+
+// 当前分支
+var cb = (execSilent('git branch').stdout.match(/(?<=\* ).*/g) || [])[0]
+// 当前时间
+var ctime = nowDate()
+// 本地分支列表
+var lbs = []
+// 远程分支列表
+var rbs = []
+// 当前分支存在远程分支
+var isrb = false
+// 修复分支前缀
+const fbPrefix = `ggit-fix-`
+// 配置文件路径
+const configPath = path.join(__dirname, 'config.json')
+
+// 初始化
+function initData() {
+  // 检查是否安装了 git
+  execSilentCheck('git --version', { errMsg: '请检查 Git 是否安装！' })
+  // 检查是否支持 Git 仓库
+  execSilentCheck('git branch', { errMsg: '请检查当前项目是否支持了 Git 仓库！' })
+  // 同步 Git 最新数据
+  execSilent('get fetch')
+  // 本地分支列表
+  lbs = execSilentCheck('git branch').stdout.match(/(?<=  ).*?(?=[ |\n])/g) || []
+  // 把当前分支添加进去
+  lbs.push(cb)
+  // 远程分支列表
+  rbs = execSilentCheck('git branch -r').stdout.match(/(?<=\/).*?(?=[ |\n])/g) || []
+  // 当前分支存在远程分支
+  isrb = rbs.includes(cb)
+  // 移除修复分支
+  delFixBranch(rbs)
+}
+
+// 获取当前时间
+function nowDate() {
+  var date = new Date()
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+  if (month >= 1 && month <= 9) { month = '0' + month }
+  if (day >= 0 && day <= 9) { day = '0' + day }
+  if (hour >= 0 && hour <= 9) { hour = '0' + hour }
+  if (minute >= 0 && minute <= 9) { minute = '0' + minute }
+  if (second >= 0 && second <= 9) { second = '0' + second }
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`
+}
+
+// ================================================== pull
+
+program
+  // 命令
+  .command('pull')
+  // 描述
+  .description('同步当前分支的远程数据，如果未建立远程分支跟踪，会自动跟踪远程分支，并拉取最新数据')
+  // 事件
+  .action((option) => {
+    // 返回分支
+    const gb = option.go
+    // 输出日志
+    BgSuccess('========================================== 开始拉取 ==========================================')
+    // 初始化
+    initData()
+    // 跟踪远程分支
+    execSilent(`git branch --set-upstream-to=origin/${cb} ${cb}`)
+    // 拉取分支
+    pullBranch(cb)
+    // 执行完成
+    BgSuccess('========================================== 拉取结束 ==========================================')
+  })
+
+// ================================================== push
+
+program
+  // 命令
+  .command('push')
+  // 描述
+  .description('提交当前分支到远程仓库，并可在提交完成后，自动切换到指定分支')
+  // 配置
+  .option('-g, --go [branch]', '合并提交结束后，切换到指定分支')
+  .option('-s, --stash [type]', '使用 stash 暂存区方式合并代码，如手动终止脚本、执行失败停止脚本，需检查是否执行了 $ git stash pop 命令，没有执行需要手动执行放出暂存区的代码，以免丢失', config.stash)
+  .option('-m, --message [msg]', '提交日志信息', `${ctime} 提交优化`)
+  // 事件
+  .action((option) => {
+    // 返回分支
+    const gb = option.go
+    // 输出日志
+    BgSuccess('========================================== 开始提交 ==========================================')
+    // 初始化
+    initData()
+    // 提交当前分支
+    pushCurrentBranch(option)
+    // 指定分支有值
+    if (gb) {
+      // 切换到指定分支
+      goBranch(gb)
+    }
+    // 执行完成
+    BgSuccess('========================================== 提交结束 ==========================================')
+  })
+
+// ================================================== merge
+
+program
+  // 命令
+  .command('merge')
+  // 描述
+  .description('提交当前分支到远程仓库，并合并到指定分支，再返回当前分支/指定分支')
+  // 配置
+  .option('-t, --to [branch]', '合并到指定的分支', config.to)
+  .option('-g, --go [branch]', '合并提交结束后，切换到指定分支', cb)
+  .option('-s, --stash [type]', '使用 stash 暂存区方式合并代码，如手动终止脚本、执行失败停止脚本，需检查是否执行了 $ git stash pop 命令，没有执行需要手动执行放出暂存区的代码，以免丢失', config.stash)
+  .option('-m, --message [msg]', '提交日志信息', `${ctime} 提交优化`)
+  // 事件
+  .action((option) => {
+    // 输出日志
+    BgSuccess('========================================== 开始合并 ==========================================')
+    // 同步冲突值
+    optionValue(['-g', '--go'], option, 'go')
+    // 初始化
+    initData()
+    // 提交当前分支
+    pushCurrentBranch(option)
+    // 合并到指定分支
+    mergeToBranch(option)
+    // 执行完成
+    BgSuccess('========================================== 合并结束 ==========================================')
+  })
+
+// ================================================== fix
+
+program
+  // 命令
+  .command('fix')
+  // 描述
+  .description('目前主要作用于修复分支偏移问题')
+  // 配置
+  .option('-p, --push [type]', '修复完成后，将代码提交到远程分支', true)
+  .option('-g, --go [branch]', '合并提交结束后，切换到指定分支')
+  .option('-m, --message [msg]', '提交日志信息', `${ctime} 修复分支偏移`)
+  // 事件
+  .action((option) => {
+    // 输出日志
+    BgSuccess('========================================== 开始修复 ==========================================')
+    // 初始化
+    initData()
+    // 修复分支偏移
+    fixBranch(option)
+    // 执行完成
+    BgSuccess('========================================== 修复结束 ==========================================')
+  })
+
+// ================================================== config
+
+program
+  // 命令
+  .command('config')
+  // 描述
+  .description('修改/查看默认配置')
+  // 配置
+  .option('set', '修改默认配置')
+  .option('get', '查看默认配置')
+  .option('-t, --to [branch]', '合并到指定的分支')
+  .option('-s, --stash [type]', '是否使用 stash 暂存区合并状态')
+  // 事件
+  .action((option, cmd) => {
+    // 检查配置类型
+    if (cmd.args.includes('set')) {
+      // 获取所有字段
+      const keys = Object.keys(option || {})
+      // 字段列表有值
+      if (keys.length) {
+        // 组装输出
+        keys.forEach((key) => {
+          // 输出
+          config[key] = option[key]
+        })
+      } else {
+        // 提示
+        console.log('缺少需要配置的参数')
+        // 停止
+        exit(1)
+      }
+      // 转成 json 字符串
+      const json = JSON.stringify(config, null, '\t')
+      // 写入配置
+      fs.writeFile(configPath, json, 'utf8', (err) => {
+        if (!err) {
+          // 成功
+          console.log(json)
+          console.log('Set Success!')
+        } else {
+          // 失败
+          console.log('修改失败')
+        }
+      })
+    } else {
+      // 获取所有字段
+      const keys = Object.keys(option || {})
+      // 字段列表有值
+      if (keys.length) {
+        // 组装输出
+        keys.forEach((key) => {
+          // 输出
+          console.log(config[key])
+        })
+      } else {
+        // 转成 json 字符串
+        const json = JSON.stringify(config, null, '\t')
+        // 输出所有配置
+        console.log(json)
+      }
+    }
+  })
+
+// ================================================== 封装方法
+
+// 提交当前分支
+function pushCurrentBranch(option) {
+  // 输出日志
+  BgWarning(`==== 注意：如果当前工程使用了第三方提交插件(例：cz 等)，会拦截命令导致无法提交，请直接使用第三方插件提交！ `)
+  // 本轮暂存区是否有代码
+  var isStash = false
+  // 需要提交代码（可能刚才解决冲突）
+  var isNeedsMerge = false
+  // 是否使用暂存区 && 有远程分支
+  if (isTure(option.stash) && isrb) {
+    // 输出日志
+    BgWarning(`==== 注意：使用 stash 暂存代码，在有保存本地内容的情况下，如手动终止脚本、执行失败停止脚本，需检查是否执行了 $ git stash pop 命令，没有执行需要手动执行放出暂存区的代码，以免丢失！`)
+    // 输出日志
+    BgInfo(`========================================== git stash`)
+    // 暂存代码
+    const stashStdout = exec('git stash').stdout
+    // 有内容暂存了
+    if (stashStdout.includes('保存工作目录和索引状态') || stashStdout.includes('Saved working directory and index state')) {
+      isStash = true
+    }
+    // 需要提交代码
+    if (stashStdout.includes('needs merge')) {
+      isNeedsMerge = true
+    }
+    // 暂存代码后
+    if (isStash) {
+      // 拉取最新代码
+      pullBranch(cb, () => {
+        // 输出日志
+        BgInfo(`========================================== git stash pop`)
+        // 释放暂存代码
+        execCheck(`git stash pop`)
+      })
+    }
+  }
+  // 输出日志
+  BgInfo(`========================================== git add .`)
+  // 添加所有文件
+  exec('git add .')
+  // 输出日志
+  BgInfo(`========================================== git commit -m "${option.message}"`)
+  // 提交到本地
+  exec(`git commit -m "${option.message}"`)
+  // 有远程分支 && 无暂存代码 && 无需合并 && 非修复状态
+  if (isrb && !isStash && !isNeedsMerge && !option.fix) {
+    // 拉取最新代码
+    pullBranch(cb)
+  }
+  // 输出日志
+  BgInfo(`========================================== git push origin ${cb}`)
+  // 提交到远程分支
+  execCheck(`git push origin ${cb}`)
+}
+
+// 合并到指定分支
+function mergeToBranch(option) {
+  // 返回分支
+  const gb = option.go
+  // 目标分支
+  const tb = option.to
+  // 当前是否在目标分支
+  if (cb != tb) {
+    // 切换到指定目标分支
+    goBranch(tb)
+    // 拉取最新代码
+    pullBranch(tb)
+    // 输出日志
+    BgInfo(`========================================== git merge ${cb}`)
+    // 合并当前分支
+    execCheck(`git merge ${cb}`)
+    // 输出日志
+    BgInfo(`========================================== git push origin ${tb}`)
+    // 提交到远程分支
+    execCheck(`git push origin ${tb}`)
+    // 切换回到开发分支
+    goBranch(gb)
+  } else {
+    // 指定分支有值
+    if (gb) {
+      // 切换到指定分支
+      goBranch(gb)
+    }
+  }
+}
+
+// 拉取分支
+function pullBranch(cb, callback) {
+  // 输出日志
+  BgInfo(`========================================== git pull origin ${cb}`)
+  // 拉取最新代码
+  const cmd = `git pull origin ${cb}`
+  const res = exec(cmd)
+  // 拉取失败，根据情况处理
+  if (res.code != 0) {
+    // 没有远程分支
+    if (res.stderr.includes('无法找到远程引用') || res.stderr.includes("couldn't find remote ref")) {
+      // 继续执行脚本
+      if (callback) { callback(res) }
+    } else {
+      // 输出错误提示
+      showError(cmd)
+      // 回调
+      if (callback) { callback(res) }
+      // 结束脚本
+      exit(res.code)
+    }
+  } else {
+    // 回调
+    if (callback) { callback(res) }
+  }
+}
+
+// 修复分支偏移
+function fixBranch(option) {
+  // 有远程分支，才允许修复
+  if (isrb) {
+    // 修复分支
+    const fb = `${fbPrefix}${cb}`
+    // 新建修复分支
+    newBranch(fb)
+    // 移除当前分支
+    delBranch(cb)
+    // 拉取当前分支
+    goBranch(cb)
+    // 拉取最新代码
+    pullBranch(cb)
+    // 输出日志
+    BgInfo(`========================================== git merge ${fb}`)
+    // 合并修复分支
+    execCheck(`git merge ${fb}`)
+    // 移除修复分支
+    delFixBranch(rbs, true)
+    // 修复完成提交远程
+    if (isTure(option.push)) {
+      // 为修复分支
+      option.fix = true
+      // 提交当前分支
+      pushCurrentBranch(option)
+    }
+  } else {
+    // 输出日志
+    BgError(`========================================== Error：${cb} 不存在远程分支，无法进行修复！`)
+    // 结束脚本
+    exit(1)
+  }
+}
+
+// 移除修复分支
+function delFixBranch(rbs, force) {
+  // 修复分支
+  const fb = `${fbPrefix}${cb}`
+  // (本地存在修复分支 && 远程不存在修复分支) || 强制
+  if ((lbs.includes(fb) && !rbs.includes(fb)) || force) {
+    // 移除修复分支
+    delBranch(fb)
+  }
+}
+
+// 移除分支（isr：远程/本地）
+function delBranch(cb, isr) {
+  if (isr) {
+    // 输出日志
+    BgInfo(`========================================== git push origin -d ${cb}`)
+    // 移除远程分支
+    execCheck(`git push origin -d ${cb}`)
+  } else {
+    // 输出日志
+    BgInfo(`========================================== git branch -D ${cb}`)
+    // 移除本地分支
+    execCheck(`git branch -D ${cb}`)
+  }
+}
+
+// 新建分支
+function newBranch(cb) {
+  // 输出日志
+  BgInfo(`========================================== git checkout -b ${cb}`)
+  // 新建分支
+  execCheck(`git checkout -b ${cb}`)
+}
+
+// 切换分支
+function goBranch(cb) {
+  // 输出日志
+  BgInfo(`========================================== git checkout ${cb}`)
+  // 切换/拉取分支
+  execCheck(`git checkout ${cb}`)
+}
+
+// 执行其他命令
+function execArgs(args) {
+  // 数组有值
+  if (args && args.length) {
+    // git 命令拼接
+    const gc = `git ${args.join(' ')}`
+    // 输出日志
+    BgInfo(`========================================== ${gc}`)
+    // 执行命令
+    exec(gc)
+  }
+}
+
+// 获取指令中的参数，当遇到重复指令冲突时，可使用进行获取
+function optionValue(keys, option, key) {
+  // 参数列表
+  const argv = process.argv
+  // 索引列表
+  const indexs = []
+  keys.forEach(key => {
+    indexs.push(argv.lastIndexOf(key))
+  })
+  // 最后的索引
+  const index = Math.max(...indexs)
+  // 是否有值
+  if (index != -1) {
+    // 取值
+    const value = argv[index + 1] || true
+    // 设置
+    option[key] = value
+    // 返回
+    return value
+  }
+  // 没值
+  return false
+}
+
+// ================================================== 收尾工作
+
+program
+  // 版本信息
+  .version(version)
+  // 配置
+  .option('-v', 'output the version number')
+  .option('-d [branch]', '移除指定本地分支')
+  .option('-dr [branch]', '移除指定远程分支')
+  .option('-b [branch]', '以当前分支为基础，新建分支')
+  .option('-g, --go [branch]', '切换到指定分支，如本地没有会拉取远程分支')
+  // 事件
+  .action((opts, cmd) => {
+    // 区分属性
+    if (opts.v) {
+      // 版本号
+      console.log(version)
+    } else if (opts.go) {
+      // 切换分支
+      goBranch(opts.go)
+    } else if (opts.b) {
+      // 新开分支
+      newBranch(opts.b)
+    } else if (opts.d) {
+      // 移除本地分支
+      delBranch(opts.d)
+    } else if (opts.Dr) {
+      // 移除远程分支
+      delBranch(opts.Dr, true)
+    } else {
+      // 执行其他命令
+      execArgs(cmd.args)
+    }
+  })
+  // 解析参数
+  .parse(process.argv)
